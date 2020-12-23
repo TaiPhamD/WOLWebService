55b04b437072d630d8e38e7828cd555549fdd2d0
Peter Pham <peterpham86@gmail.com>
2020-12-23 12:51:17 -0800

    update to use JSON for password parsing

--- wol.go
+++ wol.go
@@ -1,39 +1,53 @@
 package main
 
 import (
-	"net/http"
-	"fmt"
-	"os"
 	"bufio"
+	"encoding/json"
+	"fmt"
 	"log"
+	"net/http"
+	"os"
 )
 import "github.com/linde12/gowol"
+
 //import "strings"
 
 var Mypassword string
 var MyPort string
 var MyMAC string
+
+type auth_struct struct {
+	Password string
+}
+
 type turnonHandler struct{}
 
 func (h turnonHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	_, pass, _ := r.BasicAuth()
-	fmt.Println(pass)
-	if(pass != Mypassword){
-		fmt.Println("Password Doesnt match\n")
+	decoder := json.NewDecoder(r.Body)
+	var jsonAuth auth_struct
+
+	err := decoder.Decode(&jsonAuth)
+	if err != nil {
+		log.Print("error decoding JSON\n")
 		return
 	}
-       if packet, err := gowol.NewMagicPacket(MyMAC); err == nil {
+
+	if jsonAuth.Password != Mypassword {
+		log.Print("Password from JSON doesn't match\n")
+		return
+	}
+	if packet, err := gowol.NewMagicPacket(MyMAC); err == nil {
 		fmt.Println("Sending WOL magic packet")
-                packet.Send("192.168.2.255")          // send to broadcast
-                // specify receiving port
-        }
+		packet.Send("192.168.2.255") // send to broadcast
+		// specify receiving port
+	}
 }
 
 func main() {
 
 	file, err := os.Open("config.txt")
-        if err != nil {
-          log.Fatal(err)
+	if err != nil {
+		log.Fatal(err)
 	}
 	scanner := bufio.NewScanner(file)
 	scanner.Scan()
@@ -44,9 +58,9 @@ func main() {
 	MyMAC = scanner.Text()
 	file.Close()
 
-	fmt.Println("My password is:",Mypassword)
-	fmt.Println("My port: ",MyPort)
-	fmt.Println("My MAC is: ",MyMAC)
-        err = http.ListenAndServe(":"+MyPort, turnonHandler{})
+	fmt.Println("My password is:", Mypassword)
+	fmt.Println("My port: ", MyPort)
+	fmt.Println("My MAC is: ", MyMAC)
+	err = http.ListenAndServe(":"+MyPort, turnonHandler{})
 
 }
