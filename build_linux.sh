0e2c4ac9375718d6466b87f50561fe680275aef3
Peter Pham <peterpham86@gmail.com>
2022-12-02 18:04:59 -0800

    strip debug symbols to make exe smaller

--- build_linux.sh
+++ build_linux.sh
@@ -7,7 +7,7 @@ else
     mkdir -p build/dist
 fi
 pushd server
-go build -o ../build/dist
+go build -ldflags="-w" -o ../build/dist
 echo "compiled binary is located in build/dist"
 # pop back to the root folder
 popd
